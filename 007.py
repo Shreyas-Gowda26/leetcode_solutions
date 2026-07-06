419bac3a70e1605ad025e7ff214a9cb190098228
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-06 08:22:00 +0530

    Create 007.py

---
+++ 007.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        neg = x < 0
+        x = abs(x)
+
+        rev = 0
+        while x:
+            rev = rev * 10 + x % 10
+            x //= 10
+
+        if neg:
+            rev = -rev
+
+        return rev if -2**31 <= rev <= 2**31 - 1 else 0
