1acdd86eaea90f2936768581677ca8544b20a8b7
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-22 21:16:35 +0530

    Remove docstring from moveZeroes method
    Removed docstring from moveZeroes method.

--- 001-500/283.py
+++ 001-500/283.py
@@ -1,8 +1,8 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
+        
         Do not return anything, modify nums in-place instead.
-        """
+        
 
         j = -1
         for i in range(len(nums)):
