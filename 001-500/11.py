f874cfc8e1660d42ed0eb7fe794d5134bc8cbd5d
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-28 20:33:16 +0530

    Update 11.py with optimal approach

--- 001-500/11.py
+++ 001-500/11.py
@@ -12,3 +12,22 @@ class Solution:
                 max_area = max(max_area, area)
 
         return max_area
+    
+
+#Optimal-Approach
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        i,j = 0, len(height)-1
+
+        while i<j:
+            width = j - i
+            h = min(height[i],height[j])
+            area = h * width
+
+            max_area = max(max_area,area)
+            if height[i]<height[j]:
+                i+=1
+            else:
+                j-=1
+        return max_area
