1ad09b8c672b8128c2c1f447406a3d0037d18d77
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-28 17:07:53 +0530

    Update 136.py with optimal approach

--- 001-500/136.py
+++ 001-500/136.py
@@ -12,3 +12,10 @@ class Solution:
             
 
     #Optimal Approach
+
+    class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for i in range(0,len(nums)):
+            xor = xor ^ nums[i]
+        return xor
