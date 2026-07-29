fbb8e58be146374d23c47b56a7d07636055f6d3f
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-29 09:04:16 +0530

    Update 001.py with better approach

--- 001-500/001.py
+++ 001-500/001.py
@@ -5,3 +5,17 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
                     return i,j
+
+                # If no pair is found
+        return -1,-1
+
+#Better approach
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
+        for i,num in enumerate(nums):
+            more = target - num
+            if more in mp:
+                return [mp[more],i]
+            mp[num]=i
+        return [-1,-1]
