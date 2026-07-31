21cb17c3473b7ed9102530f9d179fc30d6dbd93e
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-31 23:07:17 +0530

    Update 169.py with better solution

--- 001-500/169.py
+++ 001-500/169.py
@@ -11,3 +11,17 @@ class Solution:
             if cnt>b:
                 return nums[i]
         return -1
+    
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)//2
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i,j in d.items():
+            if j>n:
+                return i
