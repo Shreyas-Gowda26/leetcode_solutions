36750fa347a37bbe083a2455d3833fa83e18fae4
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-31 23:22:49 +0530

    Update 169.py with optimal solution

--- 001-500/169.py
+++ 001-500/169.py
@@ -25,3 +25,24 @@ class Solution:
         for i,j in d.items():
             if j>n:
                 return i
+            
+
+#Optimal Moore's Voting Algorithm
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt = 0
+        for i in range(len(nums)):
+            if cnt == 0:
+                cnt = 1
+                el = nums[i]
+            elif nums[i]==el:
+                cnt+=1
+            else:
+                cnt-=1
+        cnt1=0
+        for i in range(len(nums)):
+            if nums[i]==el:
+                cnt1+=1
+        if cnt1>len(nums)//2:
+            return el
+        return -1
