08498e3ea9e32ff4a22b07067e08b81286d1b026
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-08-17 00:14:25 +0530

    Update 153.py

--- 001-500/153.py
+++ 001-500/153.py
@@ -12,3 +12,25 @@ class Solution:
                 high = mid -1
                 ans = min(ans,nums[mid])
         return ans
+    
+
+#Optimised Solution
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums)-1
+        ans = float('inf')
+        while low<=high:
+            mid = (low+high)//2
+            if nums[low]<=nums[high]:
+                ans = min(ans,nums[low])
+                break
+            if nums[low]<=nums[mid]:
+                ans = min(ans,nums[low])
+                low = mid+1
+            else:
+                high = mid -1
+                ans = min(ans,nums[mid])
+        return ans
+    
