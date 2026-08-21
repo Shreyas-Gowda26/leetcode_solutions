f2cd168c86a3ce4a470f2be843f6740edf771396
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-08-21 22:54:07 +0530

    Create 069.py sqrt

---
+++ 001-500/069.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        ans = 0
+        for i in range(x+1):
+            if i*i <= x:
+                ans = i
+            else:
+                break
+        return ans
