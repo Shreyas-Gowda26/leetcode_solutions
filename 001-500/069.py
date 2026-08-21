d54740ba4b1879f7ae16e1845b6b2a2986d6429f
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-08-21 23:53:07 +0530

    Update 069.py

--- 001-500/069.py
+++ 001-500/069.py
@@ -1,3 +1,4 @@
+#Time Complexity - O(n)
 class Solution:
     def mySqrt(self, x: int) -> int:
         ans = 0
@@ -6,4 +7,20 @@ class Solution:
                 ans = i
             else:
                 break
+        return ans
+
+#Using Binary Search
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        ans = 0
+        low = 1
+        high = x
+        while low<=high:
+            mid = (low +high)//2
+            if mid * mid <= x:
+                ans = mid
+                low = mid+1
+            else:
+                high = mid - 1
+            
         return ans
