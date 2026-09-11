742eb6b39260c98b7a249f3f45b59f33ccb3fec3
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-09-11 23:38:01 +0530

    Create 152.py

---
+++ 001-500/152.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        maxi = nums[0]
+
+        for i in range(len(nums)):
+            prod = 1
+
+            for j in range(i, len(nums)):
+                prod *= nums[j]
+                maxi = max(maxi, prod)
+
+        return maxi
